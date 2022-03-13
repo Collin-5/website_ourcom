044dd9dbb73ca91b3829d2cfa8c608a3d83f1dfe
Collin-5 <collins380@gmail.com>
2022-03-13 19:49:31 +0300

    media

--- website/settings/storage.py
+++ website/settings/storage.py
@@ -27,7 +27,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 #whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
